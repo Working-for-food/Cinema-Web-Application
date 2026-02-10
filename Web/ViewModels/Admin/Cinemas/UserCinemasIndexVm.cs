2be9c10dd691dbba6e1c117a76dabcbb0167bb21
anastasiia-shulhinaa <anastasiia.shulgina@knu.ua>
2026-02-10 15:26:57 +0200

    feat: implement horizontal cinema index with search and city filtering

---
+++ Web/ViewModels/Admin/Cinemas/UserCinemasIndexVm.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Web.ViewModels.Cinemas;
+
+public class UserCinemasIndexVm
+{
+    public string? SelectedCity { get; set; }
+    public string? SearchQuery { get; set; }
+    public List<SelectListItem> Cities { get; set; } = new();
+    public IEnumerable<Application.DTOs.CinemaDto> Cinemas { get; set; } = new List<Application.DTOs.CinemaDto>();
+}
