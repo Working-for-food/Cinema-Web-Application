b460ce5db1bb4067ea70d74fac61b91e922c78e0
Владислав Гладкий <vlad.v.gladkiy@gmail.com>
2026-02-12 14:03:15 +0200

    .

--- Infrastructure/Data/Migrations/20260211114109_SyncMigration2.cs
+++ Infrastructure/Data/Migrations/20260211114109_SyncMigration2.cs
@@ -11,27 +11,11 @@ namespace Infrastructure.Data.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            //migrationBuilder.AlterColumn<DateTime>(
-            //    name: "CreatedAtUtc",
-            //    table: "SavedMovies",
-            //    type: "datetime2",
-            //    nullable: false,
-            //    defaultValueSql: "SYSUTCDATETIME()",
-            //    oldClrType: typeof(DateTime),
-            //    oldType: "datetime2");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            //migrationBuilder.AlterColumn<DateTime>(
-            //    name: "CreatedAtUtc",
-            //    table: "SavedMovies",
-            //    type: "datetime2",
-            //    nullable: false,
-            //    oldClrType: typeof(DateTime),
-            //    oldType: "datetime2",
-            //    oldDefaultValueSql: "SYSUTCDATETIME()");
         }
     }
 }
